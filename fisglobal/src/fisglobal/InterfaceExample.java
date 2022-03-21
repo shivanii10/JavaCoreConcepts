d467ec8d357a9bb9ebc07f974c097c4c6afaf093
shivani <shivanisurana10@gmail.com>
2022-03-22 03:39:46 +0530

    Third Commit

--- fisglobal/src/fisglobal/InterfaceExample.java
+++ fisglobal/src/fisglobal/InterfaceExample.java
@@ -4,6 +4,18 @@ interface Shop {       // interface
 	
 	public void order(); // abstract method
 	void delivery(); // abstract method .. by default public abstract
+	public default void offer() // can only be called on inherited class.
+	{
+		System.out.println("special discount super saver ");
+	}
+	
+	// static methods can be called directly from interface,
+	//there is no object dependency.
+	
+	public static void opening()
+	{
+		System.out.println("Shop opens at 8 in the morning :) ");
+	}
 }
 
 class Garments implements Shop{
@@ -29,15 +41,23 @@ class Fruit implements Shop{
 		System.out.println("Items will be delivered in a 4-5 day.");
 	}
 	
+	@Override
+	public void offer()
+	{
+		System.out.println("Save more on fruits");
+	}
+	
 }
 
 public class InterfaceExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Shop.opening();
 		Fruit f=new Fruit();
 		f.order();
 		f.delivery();
+		f.offer();
 		
 		Garments g =new Garments();
 		g.order();
